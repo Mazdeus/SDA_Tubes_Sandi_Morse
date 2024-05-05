5915df7cf1f40ef2f2810aab296abeca65490671
Mohammad Amadeus Andika Fadhil <150133437+Mazdeus@users.noreply.github.com>
2024-05-05 19:25:19 +0700

    Update menu.c

--- src/features/menu/menu.c
+++ src/features/menu/menu.c
@@ -5,24 +5,25 @@
 void display_menu(){
     int choice;
     do {
-        printf("╔════════════════════════════╗\n");
-        printf("║   __  __                   ║\n");
-        printf("║  |  \\/  |                  ║\n");
-        printf("║  | \\  / | ___ _ __  _   _  ║\n");
-        printf("║  | |\\/| |/ _ \\ '_ \\| | | | ║\n");
-        printf("║  | |  | |  __/ | | | |_| | ║\n");
-        printf("║  |_|  |_|\\___|_| |_|\\__,_| ║\n");
-        printf("╚════════════════════════════╝\n");
+        printf("                      ╔════════════════════════════╗\n");
+        printf("                      ║   __  __                   ║\n");
+        printf("                      ║  |  \\/  |                  ║\n");
+        printf("                      ║  | \\  / | ___ _ __  _   _  ║\n");
+        printf("                      ║  | |\\/| |/ _ \\ '_ \\| | | | ║\n");
+        printf("                      ║  | |  | |  __/ | | | |_| | ║\n");
+        printf("                      ║  |_|  |_|\\___|_| |_|\\__,_| ║\n");
+        printf("                      ║                            ║\n");
+        printf("                      ╚════════════════════════════╝\n");
         printf("\n");
-        printf("  ╔══════════════════════╗\n");
-        printf("  ║     1. Show Node     ║\n");
-        printf("  ║     2. Translator    ║\n");
-        printf("  ║     3. Read File     ║\n");
-        printf("  ║     4. Info          ║\n");
-        printf("  ║     5. Exit          ║\n");
-        printf("  ╚══════════════════════╝\n");
+        printf("                         ╔══════════════════════╗\n");
+        printf("                         ║     1. Show Node     ║\n");
+        printf("                         ║     2. Translator    ║\n");
+        printf("                         ║     3. Read File     ║\n");
+        printf("                         ║     4. Info          ║\n");
+        printf("                         ║     5. Exit          ║\n");
+        printf("                         ╚══════════════════════╝\n");
         printf("\n");
-        
+        printf("Pilih Menu : ");
         scanf("%d", &choice);
         switch(choice) {
             case 1:
